6fc16b8227f04ce79ddd528bbbbdbc087fbfb30c
Rime <rimeislam672@gmail.com>
2024-02-01 01:06:15 +0600

    banner done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,21 @@
+import { Button } from "@material-tailwind/react";
+
+
+
+const Banner = () => {
+    return (
+        <div className="container h-[90vh] bg-amber-400 mx-auto flex items-center justify-center">
+            <div className=" pl-5 mt-20">
+                <h3 className="text-5xl font-bold ">Online</h3>
+                <h1 className="text-6xl font-extrabold uppercase">Movie Tickets</h1>
+                <p className="text-lg w-[500px] my-5">Elevate your movie-going experience with hassle-free online ticket booking. Explore the latest releases, secure your preferred seats, and immerse yourself in the magic of cinema, all at the click of a button. Don't miss out on the excitement – your cinematic adventure starts here!</p>
+                <Button color="deep-purple" className="font-bold">GET Ticket</Button>
+            </div>
+          <div className=" pr-5">
+          <img className="w-[720px]" src="https://i.ibb.co/92NpZMj/Screenshot-2024-02-01-002426-removebg-preview.png" alt="banner" />
+          </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,25 @@ import {
 
 const myNavbar = () => {
     return (
-        <Navbar
-          variant="gradient"
-          color="blue-gray"
-          className="mx-auto max-w-screen-xl from-blue-gray-900 to-blue-gray-800 px-4 py-3"
-        >
+      <div className="container mx-auto">
+          <Navbar
+           variant="gradient"
+           color="blue-gray"
+          style={{ color: '#333' }}
+          className="fixed z-10 rounded-none from-blue-gray-900 to-blue-gray-800 px-4 py-2 lg:px-8 lg:py-4">
+        
           <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
             <Typography
-              as="a"
-              href="#"
+            
               variant="h6"
               className="mr-4 ml-2 cursor-pointer py-1.5"
             >
-              Material Tailwind
+                  <img
+            alt="logo"
+            className="h-6 w-full object-cover object-center"
+            src="https://i.ibb.co/hFf4twf/Screenshot-2024-01-31-221510-removebg-preview.png"
+          />
+              
             </Typography>
             <div className="ml-auto flex gap-1 md:mr-4">
               <IconButton variant="text" color="white">
@@ -52,6 +58,7 @@ const myNavbar = () => {
             </div>
           </div>
         </Navbar>
+      </div>
       );
 };
 
